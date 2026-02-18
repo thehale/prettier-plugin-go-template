31a1783b90cf2b8a26b6ddbcf20aaf1a06765398
Joseph Hale <me@jhale.dev>
2026-02-17 20:40:50 -0700

    Improve tests readability

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,16 +3,6 @@ import { join } from "path";
 import * as prettier from "prettier";
 import * as GoTemplatePlugin from "./index";
 
-const prettify = (
-  code: string,
-  options: Partial<GoTemplatePlugin.PrettierPluginGoTemplateParserOptions>,
-) =>
-  prettier.format(code, {
-    parser: "go-template",
-    plugins: [GoTemplatePlugin],
-    ...options,
-  });
-
 const testFolder = join(__dirname, "tests");
 const tests = readdirSync(testFolder);
 
@@ -22,28 +12,34 @@ describe("format", () => {
       const path = join(testFolder, test);
       const input = readFileSync(join(path, "input.html")).toString();
       const expected = readFileSync(join(path, "expected.html")).toString();
+      const config = getConfig(path);
 
-      const configPath = join(path, "config.json");
-      const configString =
-        existsSync(configPath) && readFileSync(configPath)?.toString();
-      const configObject = configString ? JSON.parse(configString) : {};
-
-      const expectedError = expected.match(/Error\("(?<message>.*)"\)/)?.groups
-        ?.message;
-
-      const format = () => prettify(input, configObject);
+      const expectedError = expected.match(/Error\("(?<message>.*)"\)/)?.groups?.message;
 
       if (expectedError) {
         jest.spyOn(console, "error").mockImplementation();
-        await expect(format()).rejects.toEqual(new Error(expectedError));
+        await expect(prettify(input, config)).rejects.toEqual(new Error(expectedError));
       } else {
-        const result = prettify(input, configObject);
-        await expect(await result).toEqual(expected);
-        // Check that a second prettifying is not changing the result again.
-        await expect(await prettify(await result, configObject)).toEqual(
-          expected,
-        );
+        const result = await prettify(input, config);
+        expect(result).toEqual(expected);
+
+        // A prettified output should change when prettified again.
+        const repeatedResult = await prettify(result, config)
+        expect(repeatedResult).toEqual(expected);
       }
     }),
   );
 });
+
+function getConfig(path: string) {
+  const configPath = join(path, "config.json");
+  const configString = existsSync(configPath) && readFileSync(configPath)?.toString();
+  return configString ? JSON.parse(configString) : {};
+}
+
+function prettify(
+  code: string,
+  options: Partial<GoTemplatePlugin.PrettierPluginGoTemplateParserOptions>,
+) {
+  return prettier.format(code, { parser: "go-template", plugins: [GoTemplatePlugin], ...options });
+}
